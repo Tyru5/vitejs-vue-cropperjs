53f4c007e6d70640c303fd1d6dfa1004f669e0b1
Clayton Kinder <ClaytonAlanKinder@gmail.com>
2022-10-24 11:22:48 -0600

    More progress.

--- package/src/class.js
+++ package/src/class.js
@@ -1,5 +1,24 @@
+import _ from 'lodash';
 class CtrlAltElite {
   constructor(elementSelector, options = {}) {
-    
+    this.updateElement(elementSelector);
+    this.updateOptions(options);
+  }
+
+  updateElement(elementSelector) {
+    if (
+      !_.isElement(elementSelector)
+      && (
+        !_.isString(elementSelector)
+        || !document.querySelector(elementSelector)
+      )
+    ) {
+      throw Error('The first argument for the CtrlAltElite class must be an HTML element or a valid HTML element selector string.');
+    }
+    if (_.isElement(elementSelector)) {
+      this.element = elementSelector;
+    } else {
+      this.element = document.querySelector(elementSelector);
+    }
   }
 }
