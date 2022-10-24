bbfa52c75029f94c0c6936e993cf04b7127f5b81
M <P>
2022-10-24 15:55:46 -0600

    Created NavBar component and tweaked initial page layout and style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <script setup>
-
+import NavBar from './components/NavBar.vue';
 // Custom image element for redner the CropperJS instance:
 import ImageElement from './components/ImageElement.vue';
 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-    <a href="https://fengyuanchen.github.io/cropperjs/" target="_blank">
-      <img src="./assets/cropperjs2_logo.svg" class="logo cropperjs" alt="CropperJS logo" />
-    </a>
-  </div>
-  <ImageElement msg="Vite + Vue + CropperJS" image-ref="../src/assets/ty.jpg"/>
+  <NavBar></NavBar>
+  <ImageElement msg="Vite + Vue + CropperJS" image-ref="../src/assets/ty.jpg" />
 </template>
 
 <style scoped>
 .logo {
-  height: 6em;
+  height: 2em;
   padding: 1.5em;
   will-change: filter;
 }
